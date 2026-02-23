fdf88144b06cb57c2089a82fccfe4e97ee8490b7
Jailen-Truitt <jailen.johnson-truitt@students.dominican.edu>
2026-02-23 14:14:28 -0800

    Search Spaces updated

--- src/POPOSList.js
+++ src/POPOSList.js
@@ -1,30 +1,44 @@
 import POPOSSpace from './POPOSSpace'
 import './POPOSList.css'
 import data from './sfpopos-data.json'
+import { useState } from 'react'
 
 function POPOSList() {
-  const spaces = data.map(({ title, address, images, hours }, i) => {
-  return (
-    <div className="POPOSList">
-      <POPOSSpace
-        id={i}
-        key={title}
-        name={title}
-        address={address}
-        image={images[0]}
-        hours={hours}
-      />
-      <POPOSSpace />
-      <POPOSSpace />
-      <POPOSSpace />
-      <POPOSSpace />
-      <POPOSSpace />
-    </div>
-  )
-})
+  const [query, setQuery] = useState('')
+
+  const spaces = data
+    .filter((obj) => {
+      const inTitle = obj.title.toLowerCase().includes(query.toLowerCase())
+      const inAddress = obj.address.toLowerCase().includes(query.toLowerCase())
+      return inTitle || inAddress
+    })
+    .map((obj) => {
+      const { id, title, address, images, hours } = obj
+
+      return (
+        <POPOSSpace
+          id={id}
+          key={`${title}-${id}`}
+          name={title}
+          address={address}
+          image={images[0]}
+          hours={hours}
+        />
+      )
+    })
+    
 
 return (
     <div className="POPOSList">
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          value={query}
+          placeholder="Search by title or address"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+
       {spaces}
     </div>
   )
